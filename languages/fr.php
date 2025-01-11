69d136c09081e4270a6c7dd1f14bbde34f9f8b92
Benjamin Toussaint <benjamin.toussaint@patoo.be>
2025-01-11 22:17:23 +0100

    Add French translation

---
+++ languages/fr.php
@@ -0,0 +1,31 @@
+<?php
+
+$french = array(
+
+	'efv:settings:mime_remap' => 'Laisser File Viewer détecter à nouveau les types MIME pour les fichiers MS Office',
+	'efv:settings:enable_ffmpeg' => 'Activer la conversion de fichiers vidéo et audio en formats compatibles web en utilsant "ffmpeg"',
+	'efv:settings:ffmpeg_path' => 'Indiquer l\emplacement de la bibliothèque "ffmpeg" sur votre serveur, si ça ne fonctionne pas par défaut',
+	'efv:settings:ffprobe_path' => 'Indiquer l\emplacement de la bibliothèque "ffprobe" sur votre serveur, si ça ne fonctionne pas par défaut',
+	
+	'efv:app:google' => 'Visualisateur Google Docs',
+	'efv:app:viewerjs' => 'Visualisateur auto-hébergé (Viewer.js)',
+	'efv:app:msoffice' => 'Visualisateur web MS Office',
+	'efv:app:projekktor' => 'Lecteur auto-hébergé (Video.js)',
+	'efv:app:syntaxhighlighter' => 'En ligne (Colorisation syntaxique Prism)',
+	'efv:app:divx' => 'Lecteur web DivX',
+	'efv:app:none' => 'Pas d\'aperçu',
+	
+	'efv:type:pdf' => 'PDF',
+	'efv:type:msoffice' => 'Fichiers Microsoft Office',
+	'efv:type:odf' => 'Format Open Document',
+	'efv:type:media' => 'Vidéo/Audio',
+	'efv:type:text' => 'Texte',
+	'efv:type:code' => 'Code',
+	'efv:type:graphic' => 'Images',
+	'efv:type:postscript' => 'Postcript',
+	'efv:type:archive' => 'Archive',
+	'efv:type:other' => 'Divers',
+	
+);
+
+add_translation("fr", $french);
